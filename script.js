07088487f4de2ac79f33102c6c31a57266a36e2d
Sami Ullah <samiullahiqbal5@gmail.com>
2024-05-17 03:13:25 +0500

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const students = {
-  firstName: "Sami",
-  secondName: "Sami",
-  className: 12,
-  rollNo: 520,
-  idCard: 34101 - 3276843 - 3,
-  natioNality: "pakistan",
+let isTrue = true;
+
+// This method is use how to responsive Header through JS (SAME)
+
+// const barSet = document.querySelector(".fa-solid ");
+// console.log(barSet);
+
+// barSet.onclick = () => {
+//   const getLinks = document.querySelector(".header-links");
+//   if (isTrue == true) {
+//     getLinks.className = "header-links mobile";
+//     console.log(isTrue, "if part");
+//     isTrue = false;
+//   } else {
+//     getLinks.className = "header-links";
+//     console.log(isTrue, "else part");
+
+//     isTrue = true;
+//   }
+// };
+
+// SAME
+const barSet = document.querySelector(".fa-bars");
+console.log(barSet);
+const setNavLinks = document.querySelector(".header-links");
+
+barSet.onclick = () => {
+  if (isTrue == true) {
+    setNavLinks.classList = "header-links mobile";
+    isTrue = false;
+  } else {
+    setNavLinks.classList = "header-links";
+    isTrue = true;
+  }
 };
-if (students.firstName == students.secondName) {
-  console.log("true");
-} else {
-  console.log("false");
-}
