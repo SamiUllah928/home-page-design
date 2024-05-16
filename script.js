ebe62f0f8539fc17bcd3e0a43a569479146c659e
Sami Ullah <samiullahiqbal5@gmail.com>
2024-05-16 14:02:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const students = {
+  firstName: "Sami",
+  secondName: "Sami",
+  className: 12,
+  rollNo: 520,
+  idCard: 34101 - 3276843 - 3,
+  natioNality: "pakistan",
+};
+if (students.firstName == students.secondName) {
+  console.log("true");
+} else {
+  console.log("false");
+}
